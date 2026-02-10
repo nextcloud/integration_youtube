e5fedcc2ab97e0628d3f4de693f7cde738c507cb
Anupam Kumar <kyteinsky@gmail.com>
2026-02-10 16:46:12 +0530

    feat: moderize personal and admin settings
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,18 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { recommended } from '@nextcloud/eslint-config'
+import { recommended, recommendedJavascript } from '@nextcloud/eslint-config'
 
 export default [
 	...recommended,
+	...recommendedJavascript,
 
 	{
 		name: 'youtube/rule-overrides',
 		rules: {
 			'no-console': ['warn', { allow: ['debug', 'warn', 'error'] }],
+			'vue/v-on-event-hyphenation': 'off',
+			'vue/attribute-hyphenation': 'off',
 		},
 		files: ['src/**/*.vue', 'src/**/*.js'],
 	},
